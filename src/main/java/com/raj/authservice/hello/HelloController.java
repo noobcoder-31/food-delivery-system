1eeced3189eda08387314355019857baf09d9608
Raj Karan Singh Tomar <you@example.com>
2026-08-08 12:14:04 +0530

    Initialize auth service

---
+++ src/main/java/com/raj/authservice/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.raj.authservice.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello from first API";
+    }
+}
